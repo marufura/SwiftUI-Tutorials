126232631384a8f26fdd2ce2aa9c4e6585ef8f08
tara <yuma_takai@r.recruit.co.jp>
2024-08-17 02:18:03 +0900

    Complete: Building lists and navigation

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 高井　悠宇 on 2024/08/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
