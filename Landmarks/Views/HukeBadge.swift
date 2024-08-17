29384fb9fdcf51d15a68b63fa6f5b455617031b6
tara <yuma_takai@r.recruit.co.jp>
2024-08-18 03:57:59 +0900

    Complete: Working with UI controls

---
+++ Landmarks/Views/HukeBadge.swift
@@ -0,0 +1,31 @@
+//
+//  HukeBadge.swift
+//  Landmarks
+//
+//  Created by 高井　悠宇 on 2024/08/18.
+//
+
+import SwiftUI
+
+
+struct HikeBadge: View {
+    var name: String
+
+
+    var body: some View {
+        VStack(alignment: .center) {
+            Badge()
+                .frame(width: 300, height: 300)
+                .scaleEffect(1.0 / 3.0)
+                .frame(width: 100, height: 100)
+            Text(name)
+                .font(.caption)
+                .accessibilityLabel("Badge for \(name).")
+        }
+    }
+}
+
+
+#Preview {
+    HikeBadge(name: "Preview Testing")
+}
